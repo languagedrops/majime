8443bfb7384f1ef4ee5d39a62124d13e152d56ef
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2018-05-31 23:26:03 +0100

    feat(Array): added some global Array utility functions

---
+++ src/array.ts
@@ -0,0 +1,26 @@
+interface Array<T> {
+  remove(elem: T): T[]
+  flatten(): T[]
+  removeLastElement(): T[]
+}
+
+if (!Array.prototype.remove) {
+  Array.prototype.remove = function<T>(elem: T): T[] {
+      return this.filter((e) => e !== elem)
+  }
+}
+
+if (!Array.prototype.flatten) {
+  Array.prototype.flatten = function<T>(): T[] {
+      return [].concat(...this)
+  }
+}
+
+if (!Array.prototype.removeLastElement) {
+  Array.prototype.removeLastElement = function<T>(): T[] {
+    if (this.length === 0) {
+      return this
+    }
+    return this.slice(0, this.length - 1)
+  }
+}
