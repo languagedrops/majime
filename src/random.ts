e2589833f4ac828eb234f483888e403d9131c69d
Daniel Falus <daniel.falus@languagedrops.com>
2019-01-22 12:33:39 +0100

    feature(random) - add getRandomWithExceptions function

--- src/random.ts
+++ src/random.ts
@@ -2,6 +2,14 @@ export const getRandom = (floor: number, ceiling: number): number => {
   return Math.floor(Math.random() * (ceiling - floor + 1)) + floor
 }
 
+export const getRandomWithExceptions = (floor: number, ceiling: number, exceptions: number[]): number => {
+  const random = getRandom(floor, ceiling)
+  if (exceptions.includes(random)) {
+    return getRandomWithExceptions(floor, ceiling, exceptions)
+  }
+  return random
+}
+
 // expects a number between 0-1
 export const getRandomBoolean = (weight?: number): boolean => {
   return getRandom(1, 10000) < (weight || 0.5) * 10000
