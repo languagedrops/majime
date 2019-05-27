fd69930f906fe2db97e51bfc580da6b07b9144cc
Anton Semenov <anta.semenov@gmail.com>
2019-05-27 09:01:29 +0500

    feature(Stirngs): add lower first function

--- dist/strings.d.ts
+++ dist/strings.d.ts
@@ -1,3 +1,4 @@
 export declare const upperFirst: (source: string) => string;
+export declare const lowerFirst: (source: string) => string;
 export declare const wordUpperFirst: (source: string) => string;
 export declare const mapWords: (source: string, mapper: (value: string) => string) => string;
