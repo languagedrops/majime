753460574278a3a5209b2865e8f714cf01eca0aa
Lucas <lucas.jacobson@hotmail.de>
2019-11-18 16:29:22 +0100

    feature(Promise): add promise with timeout

--- dist/promise.d.ts
+++ dist/promise.d.ts
@@ -1,2 +1,3 @@
 export declare const promiseSequenceForEach: <T, R>(inputArray: T[], callback: (element: T, index: number, array: T[]) => Promise<R>) => Promise<void>;
 export declare const promiseSequenceMap: <T, R>(inputArray: T[], transformer: (element: T, index: number, array: T[]) => Promise<R>) => Promise<R[]>;
+export declare const promiseWithTimeoutResolved: <T, K>(promise: Promise<T>, returnValueOnTimeout: K, timeoutInMs?: number) => Promise<T | K>;
--- dist/promise.js
+++ dist/promise.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const standard_1 = require("./standard");
 exports.promiseSequenceForEach = async (inputArray, callback) => {
     for (let i = 0; i < inputArray.length; i++) {
         await callback(inputArray[i], i, inputArray);
@@ -11,4 +12,11 @@ exports.promiseSequenceMap = async (inputArray, transformer) => {
         newArray[i] = await transformer(inputArray[i], i, inputArray);
     }
     return newArray;
+};
+exports.promiseWithTimeoutResolved = async (promise, returnValueOnTimeout, timeoutInMs = 1000) => {
+    const timeout = standard_1.delay(timeoutInMs, returnValueOnTimeout);
+    return Promise.race([
+        promise,
+        timeout,
+    ]);
 };
